d840ab5eed4bc76cf7737e70effa8bc982314f10
leviatan-II <mvtias.miranda25@gmail.com>
2025-06-29 23:15:52 -0400

    login y register

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello, World!');
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Worldddd!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
